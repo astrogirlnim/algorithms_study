f2cf3a09366c776b6fa2064e51412cd535d754d6
Nataly <natybegins@gmail.com>
2022-12-11 17:11:32 -0600

    Day 5 puzzle 2

--- Algo_problem_solutions/Advent_of_Code_Dec2022/day_5.py
+++ Algo_problem_solutions/Advent_of_Code_Dec2022/day_5.py
@@ -22,3 +22,27 @@ with open("day5_input.txt","r") as f:
 
     for i in range(1,max(stacks.keys())+1):
         print(stacks[i][0])
+
+## Puzzle 2
+with open("day5_input.txt","r") as f:
+    stacks = collections.defaultdict(list)
+    for line in f:
+        if line != "\n" and line[0:4] != 'move':
+            # Reading input
+            line = line.replace("\n","")
+            l = re.split(r"\s\s\s\s|\]\s\[|\s\s\[|]\s\s",line)
+            l = [re.findall(r"[A-Z]",x) for x in l]
+ 
+            if len(l) > 1:
+                for i in range(len(l)):
+                    if l[i]:
+                        stacks[i+1].append(l[i][0])
+        elif line[0:4] == "move":
+            l = re.findall(r"[0-9][0-9]*",line)
+            l = [int(x) for x in l]
+            stacks[l[2]] = stacks[l[1]][:l[0]] + stacks[l[2]]
+            stacks[l[1]] = stacks[l[1]][l[0]:]
+
+    print("Part 2:")
+    for i in range(1,max(stacks.keys())+1):
+        print(stacks[i][0])
